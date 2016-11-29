4771bc1f222b956710097f42a193f0cb055d481b
Smip111 <smip@inbox.ru>
2016-11-29 17:48:04 +0300

    Изменения к ДЗ№5
    Внесены изменения в соответствии с комментариями

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -2,26 +2,32 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by admin on 27.11.2016.
  */
 public class HelperBase {
-    protected FirefoxDriver wd;
+    private FirefoxDriver wd;
+
 
     public HelperBase(FirefoxDriver wd) {
         this.wd=wd;
     }
 
     protected void click(By locator) {
-        wd.findElement(locator).click();
+        getElement(locator).click();
+    }
+
+    private WebElement getElement(By locator) {
+        return wd.findElement(locator);
     }
 
     protected void type(By locator, String text) {
-        click(locator);
-        wd.findElement(locator).clear();
-        wd.findElement(locator).sendKeys(text);
+        WebElement e = getElement(locator);
+        e.clear();
+        e.sendKeys(text);
     }
 
     public boolean isAlertPresent() {
