fdf49c45e184d7fd2b383af760ddee7ac84b06b3
Smip111 <smip@inbox.ru>
2017-01-03 11:59:17 +0200

    Contact and Group Data generators

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,14 +1,23 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+
 import java.io.File;
 
 public class ContactData {
 
 
     private int id = Integer.MAX_VALUE;
+
+    @Expose
     private String firstName;
+
+    @Expose
     private String lastName;
+
+    @Expose
     private String address;
+
     private String home;
     private String group;
     private String homePhone;
